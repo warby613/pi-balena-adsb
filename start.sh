3ab9d5ac950161b3ef7e4aa45dfdaed6695a2313
Glenn Stewart <gstewart@atlassian.com>
2017-05-29 17:03:11 +1000

    testing

--- start.sh
+++ start.sh
@@ -3,11 +3,23 @@
 env > out.txt
 env
 
+# Configure Flightaware
 if [[ ! -z $PIAWARE_USERNAME ]] && [[ ! -z $PIAWARE_PASSWORD ]]; then
   if [[ -x /usr/bin/piaware-config ]]; then
     /usr/bin/piaware-config flightaware-user ${PIAWARE_USERNAME}
     /usr/bin/piaware-config flightaware-password ${PIAWARE_PASSWORD}
   fi
+fi
+
+
+# Unload the driver module to allow access to dongle
+rmmod dvb_usb_rtl28xxu
+
+# Run Flightaware dump1090
+if [[ -x /usr/bin/dump1090-fa ]]; then
+    /usr/bin/dump1090-fa --net --gain -10 --ppm 1 --lat ${LAT} --lon ${LONG} --max-range 400 \
+               --net-ro-size 500 --net-ro-interval 1 --net-buffer 2 \
+               --stats-every 3600 --quiet &
 fi
 
 echo "This is where your application would start..."
