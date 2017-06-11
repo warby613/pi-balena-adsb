9dc2b8fa9fc6b1ce867ecf56073dd1ed75cffcea
Glenn Stewart <gstewart@atlassian.com>
2017-06-11 20:27:31 +1000

    Tidy up

--- start.sh
+++ start.sh
@@ -6,9 +6,15 @@
 # See https://flightaware.com/adsb/piaware/claim for new entries
 # See https://flightaware.com/adsb/piaware/advanced_configuration for options
 #
+# Feed Status: https://flightaware.com/adsb/stats/user/${PIAWARE_USERNAME}
+#
 # Flightaware is started by systemd
 # - /lib/systemd/system/dump1090-fa.service
 # - /lib/systemd/system/piaware.service
+
+echo ------------------------------------------
+echo FLIGHTAWARE / PIAWARE
+echo ------------------------------------------
 
 [[ ! -z ${PIAWARE_USERNAME} ]] && /usr/bin/piaware-config flightaware-user ${PIAWARE_USERNAME}
 [[ ! -z ${PIAWARE_PASSWORD} ]] && /usr/bin/piaware-config flightaware-password ${PIAWARE_PASSWORD}
@@ -23,15 +29,19 @@ echo "CONFIG: Flightaware"
 # Unload the driver module to allow access to dongle
 rmmod dvb_usb_rtl28xxu
 
-
-
 # ==========================================
 # PLANEFINDER
 # ==========================================
-# See https://planefinder.net/sharing/account
+# See https://planefinder.net/sharing/client
+#
+# Feed Status: https://planefinder.net/sharing/account
 #
 # Planefinder is started by systemd
 # - /run/systemd/generator.late/pfclient.service
+
+echo ------------------------------------------
+echo PLANEFINDER
+echo ------------------------------------------
 
 PF_CLIENT="/usr/bin/pfclient"
 PF_CLIENT_CFG="/etc/pfclient-config.json"
@@ -57,9 +67,16 @@ fi
 # ==========================================
 # FLIGHTRADAR24
 # ==========================================
+# See https://feed.flightradar24.com/fr24feed-manual.pdf
+#
+# Feed Status: https://www.flightradar24.com/account/data-sharing
 
 FR24_CLIENT="/usr/bin/fr24feed"
 FR24_CLIENT_CFG="/etc/fr24feed.ini"
+
+echo ------------------------------------------
+echo FLIGHTRADAR24
+echo ------------------------------------------
 
 if [[ -x ${FR24_CLIENT} ]] && [[ -w ${FR24_CLIENT_CFG} ]]; then
 	if [[ ! -z ${FR24_KEY} ]] && \
@@ -85,5 +102,6 @@ while true; do
   systemctl status dump1090-fa.service -l
   systemctl status piaware.service -l
   systemctl status pfclient -l
+  systemctl status fr24feed -l
   sleep 60
 done
