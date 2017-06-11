63e2750971a57583ec9930937f32cbb7879f02ad
Glenn Stewart <gstewart@atlassian.com>
2017-06-11 20:02:06 +1000

    Adding new default fr24feed.ini

--- start.sh
+++ start.sh
@@ -46,6 +46,7 @@ if [[ -x ${PF_CLIENT} ]] && [[ -w ${PF_CLIENT_CFG} ]]; then
 	else
 		echo "Missing required Planefinder variables - \$PF_SHARECODE \$LAT \$LONG"
 		echo "Connect to http://<your_rpi_ip>:30053 to config and claim a sharecode"
+		echo "Add variable PF_SHARECODE to Resin.io device variables once claimed."
 	fi
 	# Show the Planefinder configuration
 	echo "CONFIG: Planefinder"
@@ -57,11 +58,24 @@ fi
 # FLIGHTRADAR24
 # ==========================================
 
+FR24_CLIENT="/usr/bin/fr24feed"
+FR24_CLIENT_CFG="/etc/fr24feed.ini"
 
-
-
-
-
+if [[ -x ${FR24_CLIENT} ]] && [[ -w ${FR24_CLIENT_CFG} ]]; then
+	if [[ ! -z ${FR24_KEY} ]] && \
+	   [[ ! -z ${LONG} ]] && \
+	   [[ ! -z ${LAT} ]]; then
+		echo fr24key=\"$FR24_KEY\" >> ${FR24_CLIENT_CFG}
+	else
+		echo "Missing required Flightradar24 variables - \$FR24_KEY \$LAT \$LONG"
+		echo "Connect Resin.io terminal and run /usr/bin/fr24feed --signup to get a sharecode"
+		echo "Add variable FR24_KEY to Resin.io device variables once signed up."
+	fi
+	# Show the Planefinder configuration
+	echo "CONFIG: Flightradar24"
+	cat ${FR24_CLIENT_CFG}
+	service fr24feed restart
+fi
 
 # Allow everything to start before querying
 sleep 60
