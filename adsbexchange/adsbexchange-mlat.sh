9f877a43d53c7039d6e834c6eee92ccf79e1264f
Craig Steinberger <cjs@github.com>
2018-01-19 23:32:08 -0500

    correct errors in services and mlat script

--- adsbexchange/adsbexchange-mlat.sh
+++ adsbexchange/adsbexchange-mlat.sh
@@ -9,6 +9,7 @@ else
   name="${RESIN_DEVICE_UUID}"
 fi
 
+name=${ADSBEXCHANGE_NAME:-$RESIN_DEVICE_UUID}
 echo "Running with name "${name}
 
-/usr/bin/mlat-client --input-type dump1090 --input-connect localhost:30005 --lat $LAT --lon $LONG --alt $ALT --user "${name}"--server feed.adsbexchange.com:31090 --no-udp --results beast,connect,localhost:30104
+/usr/bin/mlat-client --input-type dump1090 --input-connect localhost:30005 --lat $LAT --lon $LONG --alt $ALT --user "${name}" --server feed.adsbexchange.com:31090 --no-udp --results beast,connect,localhost:30104
